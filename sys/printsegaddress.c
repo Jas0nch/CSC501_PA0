d14a002181e1e0fdd607238851ca61ada7437823
Hua Cao <hcao5@ncsu.edu>
2019-08-29 18:18:28 +0000

    modified printsegaddress.c

--- sys/printsegaddress.c
+++ sys/printsegaddress.c
@@ -4,9 +4,9 @@
 	extern unsigned short end;
         void printsegaddress()
 	{
-		printf("void printsegaddress()\n\n");
-		printf("Current: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext, etext, &edata, edata, &end, end);
-		printf("Proceding: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext-0x4, *(&etext-0x4), &edata-0x4, *(&edata-0x4), &end-2, *(&end-2));
-		printf("After: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n\n", &etext+0x4, *(&etext+0x4), &edata+0x4, *(&edata+0x4), &end+2, *(&end+2));
+		kprintf("void printsegaddress()\n\n");
+		kprintf("Current: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext, etext, &edata, edata, &end, end);
+		kprintf("Proceding: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext-0x4, *(&etext-0x4), &edata-0x4, *(&edata-0x4), &end-2, *(&end-2));
+		kprintf("After: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n\n", &etext+0x4, *(&etext+0x4), &edata+0x4, *(&edata+0x4), &end+2, *(&end+2));
 		return 0;
         }
