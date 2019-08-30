06ac489e6e6a2d9f043a698c44a48b053749ec53
Stan cao <stan.caohua@gmail.com>
2019-08-30 18:45:02 -0400

    add printprocstks to Makefile

--- h/lab0.h
+++ h/lab0.h
@@ -1,3 +1,4 @@
 extern long zfunction (long param);
 void printsegaddress();
 void printtos();
+void printprocstks(int priority);
---
+++ sys/printprocstks.c
@@ -0,0 +1,4 @@
+void printprocstks(int priority){
+    printf("this is printprocstks!");
+    return 0;
+}
