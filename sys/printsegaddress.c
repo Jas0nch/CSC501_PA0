b0e15435ad4f53ec3dca88b227521b6a70e03c30
Stan cao <stan.caohua@gmail.com>
2019-08-30 13:54:00 -0400

    change char to int in printsegaddress.c

--- sys/printsegaddress.c
+++ sys/printsegaddress.c
@@ -1,12 +1,11 @@
 	#include <lab0.h>
 
-        extern char etext, edata;
-	extern unsigned short end;
+        extern int etext, edata, end;
         void printsegaddress()
 	{
 		kprintf("void printsegaddress()\n\n");
-		kprintf("Current: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext, etext, &edata, edata, &end, end);
-		kprintf("Proceding: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n", &etext-0x4, *(&etext-0x4), &edata-0x4, *(&edata-0x4), &end-2, *(&end-2));
-		kprintf("After: etext[%8x]=%8x, edata[%8x]=%8x, end[%8x]=%8x\n\n", &etext+0x4, *(&etext+0x4), &edata+0x4, *(&edata+0x4), &end+2, *(&end+2));
+		kprintf("Current: etext[0x%8x]=0x%8x, edata[0x%8x]=0x%8x, end[0x%8x]=0x%8x\n", &etext, etext, &edata, edata, &end, end);
+		kprintf("Proceding: etext[0x%8x]=0x%8x, edata[0x%8x]=0x%8x, end[0x%8x]=0x%8x\n", &etext-0x1, *(&etext-0x1), &edata-0x1, *(&edata-0x1), &end-1, *(&end-1));
+		kprintf("After: etext[0x%8x]=0x%8x, edata[0x%8x]=0x%8x, end[0x%8x]=0x%8x\n\n", &etext+0x1, *(&etext+0x1), &edata+0x1, *(&edata+0x1), &end+1, *(&end+1));
 		return 0;
         }
