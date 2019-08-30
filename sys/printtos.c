4402ccf77bf3aa5dcc539a1d47e7058123b83462
Stan cao <stan.caohua@gmail.com>
2019-08-29 21:07:47 -0400

    first try for pointtos()

--- sys/printtos.c
+++ sys/printtos.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <lab0.h>
 
+
+static unsigned long	*esp;
+static unsigned long	*ebp;
+
 void printtos(){
 	printf("this is printtos!");
+	unsigned long	*sp, *fp;
+
+	asm("movl %esp,esp");
+	asm("movl %ebp,ebp");
+
+	sp = esp;
+	fp = ebp;
+
+	kprintf("void printtos()\n");
+	kprintf("Before[%8x]: %8x\n", ebp, *fp);
+	kprintf("After[%8x]: %8x\n", ebp+1, *(ebp+1));
+
+
 	return 0;
 }
